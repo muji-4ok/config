2d703744238e8b587f6d381a2251a4290d261bcc
Egor Kutovoy <egorkincbl@gmail.com>
2022-09-02 10:34:16 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf $(realpath .vimrc) ~/.vimrc
+ln -sf $(realpath .tmux.conf) ~/.tmux.conf
+ln -sf $(realpath .tmux.conf.local) ~/.tmux.conf.local
+
+mkdir -p ~/.config/nvim
+ln -sf $(realpath .config/nvim/init.vim) ~/.config/nvim/init.vim
