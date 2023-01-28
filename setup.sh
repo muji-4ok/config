2b3f534b401b713371789792b8b600eda4369fbc
Egor Kutovoy <egorkincbl@gmail.com>
2023-01-29 01:17:17 +0300

    Also configure git

--- setup.sh
+++ setup.sh
@@ -11,3 +11,5 @@ ln -sf $(realpath .config/nvim/init.vim) ~/.config/nvim/init.vim
 mkdir -p ~/.config/git
 ln -sf $(realpath .config/git/ignore) ~/.config/git/ignore
 
+git config --global core.excludesfile ~/.config/git/ignore
+
