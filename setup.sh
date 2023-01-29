8ea05a4f72bb9877bd4e9018c87d750d1e6d1486
Egor Kutovoy <egorkincbl@gmail.com>
2023-01-29 03:11:07 +0300

    Improve instructions

--- setup.sh
+++ setup.sh
@@ -11,8 +11,11 @@ ln -sf $(realpath .config/nvim/init.vim) ~/.config/nvim/init.vim
 mkdir -p ~/.config/git
 ln -sf $(realpath .config/git/ignore) ~/.config/git/ignore
 
-echo "Install neovim version >= 0.7!"
-echo "Install vim-plug - https://github.com/junegunn/vim-plug#unix-linux !"
+echo "Install tmux"
+echo "Install neovim version >= 0.7"
+echo "Add 'alias vim=nvim' to .bashrc"
+echo "Add 'export EDITOR=nvim' to .bashrc"
+echo "Install vim-plug - https://github.com/junegunn/vim-plug#unix-linux"
 echo "Run :PlugInstall"
 
 git config --global core.excludesfile ~/.config/git/ignore
